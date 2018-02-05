4941286d3affd4cd2f283bb5b79f2f0761347f63
AndyBrizhak <andrewbrizhak@gmail.com>
2018-02-05 08:24:41 +0300

    +add edit dep +method update

--- Employee_Department/EditDep.xaml.cs
+++ Employee_Department/EditDep.xaml.cs
@@ -9,18 +9,21 @@ namespace Employee_Department
     /// </summary>
     public partial class EditDep : Window
     {
-        public EditDep(Department _selectedDep)
+        //public EditDep(Department _selectedDep)
+        public EditDep()
         {
             InitializeComponent();
             //NameDepTextBox.Text = _selectedDep.Name;
-            UpdateInfo();
+            Update();
 
         }
 
-        public void UpdateInfo()
+        public void Update()
         {
-            DepartsComboBox.Items.Clear();
-            foreach (var ob in MainWindow.dep_list) DepartsComboBox.Items.Add(ob.Name);
+            //DepartsComboBox.Items.Clear();
+            //foreach (var ob in MainWindow.dep_list) DepartsComboBox.Items.Add(ob.Name);
+            EditDepCombobox.Items.Clear();
+            foreach (var ob in MainWindow.dep_list) EditDepCombobox.Items.Add(ob.Name);
         }
 
 
--- Employee_Department/MainWindow.xaml.cs
+++ Employee_Department/MainWindow.xaml.cs
@@ -21,9 +21,9 @@ namespace Employee_Department
     public partial class MainWindow : Window
     {
         internal static List<Department> dep_list { get; set; }
-        
-        private Department _selectedDep;
-       
+
+        //private Department _selectedDep;
+
         List<Employee> emp_list = new List<Employee>();
         private Employee _selectedEmp;
 
@@ -31,16 +31,15 @@ namespace Employee_Department
         public MainWindow()
         {
             InitializeComponent();
-            List<Department> dep_list = new List<Department>();
-            dep_list.Add(new Department() { Name = "Laundry" });       
-            dep_list.Add(new Department() { Name = "Receiving room" });
-            dep_list.Add(new Department() { Name = "Morgue" });
-            DepComboBox.Items.Clear();
-            foreach (var item in dep_list)
-            {
-                DepComboBox.Items.Add(item);
-            }
-                                
+            CreateDepartmens();
+            Update();
+
+            //DepComboBox.Items.Clear();
+            //foreach (var item in dep_list)
+            //{
+            //    DepComboBox.Items.Add(item);
+            //}
+
             emp_list.Add(new Employee("Lozhkin", dep_list[0]));
             emp_list.Add(new Employee("Pupkin", dep_list[1]));
             emp_list.Add(new Employee("Ivanov", dep_list[2]));
@@ -52,12 +51,32 @@ namespace Employee_Department
                        
         }
 
+        public void CreateDepartmens()
+        {
+            dep_list = new List<Department>()/* { d1, d2, d3 }*/;
+            dep_list.Add(new Department("Laundry"));
+            dep_list.Add(new Department("Receiving room"));
+            dep_list.Add(new Department("Morgue"));
+            //Department d1 = new Department("Laundry");
+            //Department d2 = new Department("Receiving room");
+            //Department d3 = new Department("Morgue");
+
+
+        }
+
+        public void Update()
+        {
+            DepComboBox.Items.Clear();
+            foreach (var ob in dep_list) DepComboBox.Items.Add(ob.Name);
+        }
+
         private void DepComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (DepComboBox.SelectedItem == null) return;
             var index = DepComboBox.SelectedIndex;
-            _selectedDep = dep_list[index];
-            EditDep _editDepWindow = new EditDep(_selectedDep);
+            //_selectedDep = dep_list[index];
+            //EditDep _editDepWindow = new EditDep(_selectedDep);
+            EditDep _editDepWindow = new EditDep();
             _editDepWindow.Owner = this;
             _editDepWindow.ShowDialog();
         }
